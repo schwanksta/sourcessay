154f15e205cd6226649ab0b6a19c50b02435d959
Ken Schwencke <schwank@gmail.com>
2012-07-12 00:56:23 -0700

    Added Twitter bootstrap and a quick DEBUG media server.

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic import ListView
 from sourcessay.models import Item
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,3 +10,12 @@ urlpatterns = patterns('',
     url(r'^$', ListView.as_view(model=Item, template_name="index.html"), name='index'),
     #url(r'^admin/', include(admin.site.urls)),
 )
+
+# URL patterns below here will only be live in the development environment.
+if settings.DEVELOPMENT:
+    urlpatterns += patterns("django.views",
+        url(r"^media/(.*)$", "static.serve", {
+            "document_root": settings.MEDIA_ROOT,
+            'show_indexes': True, 
+            }),
+    )
