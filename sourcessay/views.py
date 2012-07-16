04b7fd389c268eb48a0b480e04bcdf7175cff958
Ken Schwencke <schwank@gmail.com>
2012-07-15 18:34:29 -0700

    Added a quick top author and top outlet list in the rail

--- sourcessay/views.py
+++ sourcessay/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.db.models import Count
+from django.shortcuts import render
+from sourcessay.models import Item, Author, Outlet
+def index(request):
+    articles = Item.objects.all()
+    top_authors = Author.objects.all().annotate(Count('item')).order_by('-item__count')[:5]
+    top_outlets = Outlet.objects.all().annotate(Count('item')).order_by('-item__count')[:5]
+    context = dict(
+        object_list = articles,
+        top_authors = top_authors,
+        top_outlets = top_outlets
+    )
+    return render(request, "index.html", context)
--- urls.py
+++ urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic import ListView
 from sourcessay.models import Item
+from sourcessay.views import index
 from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', ListView.as_view(model=Item, template_name="index.html"), name='index'),
+    url(r'^$', index, name='index'),
     #url(r'^admin/', include(admin.site.urls)),
 )
 
