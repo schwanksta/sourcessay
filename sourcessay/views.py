17f01d512a41997c862fefb0170fd963e06d5fda
Ken Schwencke <schwank@gmail.com>
2013-07-04 00:44:28 -0700

    Let's limit her a bit.

--- sourcessay/views.py
+++ sourcessay/views.py
@@ -1,8 +1,9 @@
 from django.db.models import Count
 from django.shortcuts import render
 from sourcessay.models import Item, Author, Outlet
+
 def index(request):
-    articles = Item.objects.all()
+    articles = Item.objects.all()[:200]
     top_authors = Author.objects.all().annotate(Count('item')).order_by('-item__count')[:5]
     top_outlets = Outlet.objects.all().annotate(Count('item')).order_by('-item__count')[:5]
     context = dict(
